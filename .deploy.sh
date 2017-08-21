fdbf515a61845b3f3e0d1ec88c974d998fd139eb
Pierre Lavaux <pierre@lavaux.lv>
2017-08-21 16:49:37 +0200

    CI : Fixing the deploy script, again.

--- .deploy.sh
+++ .deploy.sh
@@ -10,14 +10,14 @@ fi
 set -e
 
 # Clone 'gh-pages' branch of the repository using encrypted GH_TOKEN for authentification.
-git clone -b gh-pages https://${GH_TOKEN}@github.com/PierreLvx/lavaux.lv.git /gh-pages_lavaux
+git clone -b gh-pages https://${GH_TOKEN}@github.com/PierreLvx/lavaux.lv.git ~/gh-pages_lavaux
 
 # Copy generated HTML site to 'gh-pages' branch.
-cp -R _site/* /gh-pages_lavaux
+cp -R _site/* ~/gh-pages_lavaux
 
 # Commit and push generated content to 'gh-pages' branch.
 # Since repository was cloned in write mode with token auth - we can push there.
-cd /PierreLvx/gh-pages_lavaux
+cd ~/gh-pages_lavaux
 git config user.email "pierre@lavaux.lv"
 git config user.name "Pierre Lavaux"
 git add -A .
